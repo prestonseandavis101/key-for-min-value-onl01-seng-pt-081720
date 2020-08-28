1a3c50f245488af50f59b301a50a6aee94f413b4
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-28 21:35:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_for_min_value.collect |name, hash_value|
+  key_for_min_value.collect|name, hash_value|
   
 
 end
